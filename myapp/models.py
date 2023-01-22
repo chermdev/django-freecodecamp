41394d262af665c775d5d74d111c46eafc468dfe
ChermDev <carlos_e8246@hotmail.com>
2023-01-22 01:12:12 -0600

    Use of models and conditionals

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from dataclasses import dataclass
 
 # Create your models here.
+
+
+@dataclass
+class Feature:
+    id: int
+    name: str
+    details: str
+    is_true: bool
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from .models import Feature
 
 # Create your views here.
 
 
 def index(request):
+    features = []
+    feature_data = (('Fast', 'Lorem ipsum dolor sit amet.', True),
+                    ('Reliable', 'Lorem ipsum dolor sit amet.', True),
+                    ('Easy to Use', 'Lorem ipsum dolor sit amet.', False),
+                    ('Affordable', 'Lorem ipsum dolor sit amet.', True))
+    for name, details, is_true in feature_data:
+        features.append(
+            Feature(id=0,
+                    name=name,
+                    details=details,
+                    is_true=is_true
+                    )
+        )
     context = {
-        'name': 'Bob',
-        'age': '53',
-        'nationality': 'British'
+        'features': features
     }
     return render(request, "index.html", context)
 
